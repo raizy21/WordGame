b4448ddb567c5340b2b9a84e93208d69fd6c16f2
raizy <39349958+raizy21@users.noreply.github.com>
2023-10-08 20:08:16 +0200

    adding styles to app

---
+++ App.styles.js
@@ -0,0 +1,54 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  root: {
+    flex:1, 
+    alignItems: "center",
+    justifyContent : "center", 
+    padding: 10,
+    paddingTop: 40,
+  },
+
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    alignSelf: "stretch",
+  },
+  
+  optionsContainer: {
+    width: "100%",
+    flex: 1,
+
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    alignContent: "space-between",
+  },
+
+  optionContainer : {
+    //border
+    borderWidth: 2,
+    borderBottomWidth: 4,
+    borderColor : "yellow",
+    borderRadius: 10,
+
+    //size
+    width: "48%",
+    height: "48%",
+
+    //spacing
+    padding:10,
+
+    alignItems: "center",
+  },
+  
+   optionImage : {
+    width: 100,
+    height: 100,
+    flex: 1,
+   },
+   optionText: {},
+
+})
+
+export default styles;
