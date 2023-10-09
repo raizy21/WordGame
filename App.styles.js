6916588363230b131e3c9b1957ccc363daa7afad
raizy <39349958+raizy21@users.noreply.github.com>
2023-10-09 10:49:35 +0200

    Update App.styles.js

--- App.styles.js
+++ App.styles.js
@@ -1,10 +1,10 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   root: {
-    flex:1, 
+    flex: 1,
     alignItems: "center",
-    justifyContent : "center", 
+    justifyContent: "center",
     padding: 10,
     paddingTop: 40,
   },
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     alignSelf: "stretch",
   },
-  
+
   optionsContainer: {
     width: "100%",
     flex: 1,
@@ -24,31 +24,6 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignContent: "space-between",
   },
-
-  optionContainer : {
-    //border
-    borderWidth: 2,
-    borderBottomWidth: 4,
-    borderColor : "yellow",
-    borderRadius: 10,
-
-    //size
-    width: "48%",
-    height: "48%",
-
-    //spacing
-    padding:10,
-
-    alignItems: "center",
-  },
-  
-   optionImage : {
-    width: 100,
-    height: 100,
-    flex: 1,
-   },
-   optionText: {},
-
-})
+});
 
 export default styles;
