8532cda6a9a74002277fee5efbb8cd95f9846b20
raizy <39349958+raizy21@users.noreply.github.com>
2023-10-10 09:23:51 +0200

    adding option

--- App.js
+++ App.js
@@ -2,16 +2,20 @@ import React from "react";
 import { Text, View } from "react-native";
 import ImageOption from "./assets/src/components/ImageOption";
 import styles from "./App.styles";
+import question from "./assets/data/QuestionWithOptions";
 
 const App = () => {
   return (
     <View style={styles.root}>
-      <Text style={styles.title}>Which of these is the "book"?</Text>
+      <Text style={styles.title}>question.question</Text>
       <View style={styles.optionsContainer}>
-        <ImageOption />
-        <ImageOption />
-        <ImageOption />
-        <ImageOption />
+        {question.options.map((option) => (
+          <ImageOption
+            key={option.id}
+            image={option.image}
+            text={option.text}
+          />
+        ))}
       </View>
     </View>
   );
