63a8a7435559b9629668a2cc818169a316ddb901
raizy <39349958+raizy21@users.noreply.github.com>
2023-10-10 09:17:48 +0200

    create QuestionWithOptions.js

---
+++ assets/data/QuestionWithOptions.js
@@ -0,0 +1,27 @@
+export default {
+  question: 'Which os these is "the book"?',
+  options : [
+    {
+      id: "option1",
+      image: "./assets/book.jpg",
+      text: "book",
+      correct: true,
+    },
+    {
+      id: "option2",
+      image: "./assets/cup.jpg",
+      text: "cup",
+    },
+    {
+      id: "option3",
+      image: "./assets/notebook.png",
+      text: 'notebook',
+    }, 
+    {
+      
+      id: "option4",
+      image: "./assets/pen.jpg",
+      text: "pen",
+    },
+  ],
+};
